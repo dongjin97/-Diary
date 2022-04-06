22d98dad7693ba6c8738be4c1348dd136f8d8f2f
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-04-06 22:17:40 +0900

    start study/dev

---
+++ Diary/Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by 원동진 on 2022/04/06.
+//start
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
